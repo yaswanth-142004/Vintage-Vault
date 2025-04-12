23f90e541886c13d2a18b9de06408c55ccae6afe
Yaswanth V <yashwanth142004@gmail.com>
2025-04-13 04:30:11 +0530

    integration of frontend and backend is done for user module and sidebar module checking is over

---
+++ Frontend/vintage_vault/src/store/slices/commissionSlice.js
@@ -0,0 +1,42 @@
+import { createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import { toast } from "react-toastify";
+const API_URL = import.meta.env.REACT_APP_API_URL;
+const commissionSlice = createSlice({
+  name: "commission",
+  initialState: {
+    loading: false,
+  },
+  reducers: {
+    postCommissionProofRequest(state, action) {
+      state.loading = true;
+    },
+    postCommissionProofSuccess(state, action) {
+      state.loading = false;
+    },
+    postCommissionProofFailed(state, action) {
+      state.loading = false;
+    },
+  },
+});
+
+export const postCommissionProof = (data) => async (dispatch) => {
+  dispatch(commissionSlice.actions.postCommissionProofRequest());
+  try {
+    const response = await axios.post(
+      `${API_URL}/api/v1/commission/proof`,
+      data,
+      {
+        withCredentials: true,
+        headers: { "Content-Type": "multipart/form-data" },
+      }
+    );
+    dispatch(commissionSlice.actions.postCommissionProofSuccess());
+    toast.success(response.data.message);
+  } catch (error) {
+    dispatch(commissionSlice.actions.postCommissionProofFailed());
+    toast.error(error.response.data.message);
+  }
+};
+
+export default commissionSlice.reducer;
